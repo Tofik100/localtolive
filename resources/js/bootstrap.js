f700f4c60be9f5b28184e4f6c56e51bd2765bad3
Tofik100 <tofik@coretechies.com>
2021-10-29 06:41:05 +0000

    npm setup

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,3 +39,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+window.Vue = require('vue');
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const app = new Vue({
+  el: '#app',
+});
